1039a8907d81090a0dfec4488f7158896c841eb7
Julia Demers <juliademers1@gmail.com>
2023-06-07 22:24:48 -0700

    tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square ({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+
+  )
 }
+
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [xIsNext, setXIsNext] = useState(true)
+
+  function handleClick(i) {
+    if (squares[i] == null && calculateWinner(squares) == null) {
+      const nextSquares = squares.slice();
+      (xIsNext ? (nextSquares[i] = 'X') : (nextSquares[i] = 'O'))
+      setSquares(nextSquares)
+      setXIsNext(!xIsNext)
+    }
+  }
+
+  function calculateWinner(table) {
+    const winners = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6], 
+      [1,4,7], 
+      [2,5,8], 
+      [0,4,8], 
+      [2,4,6]]
+
+    for (let win of winners) {
+      const [a,b,c] = win
+      if (table[a] === table[b] && table[c] === table[a] && table[a]) {
+        return true
+      }
+    }
+    return null
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  return (
+    <div>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
+      </div>
+    </div>
+  );
+}
+
+
+// Next steps: 
+// I want to reset the game 
+// I need to add better styling
+// Ill go through the rest of the tutorial
+// Change the time travel feature to be a back or forward arrow. 
